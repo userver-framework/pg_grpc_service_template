f1ce089bca9a1b636192d93e9eee4b3b575b2723
Anton Zhilin <antonyzhilin@userver.tech>
2024-02-16 13:11:11 +0300

    feat proto: use pre-generated protos in testsuite (#30)

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,9 +1,11 @@
 import pytest
 
+import handlers.hello_pb2 as hello_protos  # noqa: E402, E501
+
 # Start the tests via `make test-debug` or `make test-release`
 
 
-async def test_grpc_client(hello_protos, mock_grpc_server, grpc_service):
+async def test_grpc_client(mock_grpc_server, grpc_service):
     @mock_grpc_server('SayHello')
     async def mock_say_hello(request, context):
         assert request.name
@@ -17,13 +19,13 @@ async def test_grpc_client(hello_protos, mock_grpc_server, grpc_service):
     assert mock_say_hello.times_called == 1
 
 
-async def test_first_time_users(hello_protos, grpc_service):
+async def test_first_time_users(grpc_service):
     request = hello_protos.HelloRequest(name='userver')
     response = await grpc_service.SayHello(request)
     assert response.text == 'Hello, userver!\n'
 
 
-async def test_db_updates(hello_protos, grpc_service):
+async def test_db_updates(grpc_service):
     request = hello_protos.HelloRequest(name='World')
     response = await grpc_service.SayHello(request)
     assert response.text == 'Hello, World!\n'
@@ -38,7 +40,7 @@ async def test_db_updates(hello_protos, grpc_service):
 
 
 @pytest.mark.pgsql('db_1', files=['initial_data.sql'])
-async def test_db_initial_data(hello_protos, grpc_service):
+async def test_db_initial_data(grpc_service):
     request = hello_protos.HelloRequest(name='user-from-initial_data.sql')
     response = await grpc_service.SayHello(request)
     assert response.text == 'Hi again, user-from-initial_data.sql!\n'
