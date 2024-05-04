8b85c79ed703e2cf48c90f06bd48f83580d65ff2
root-kidik <root.kidik@gmail.com>
2024-05-04 03:24:58 +0000

    feat: separate client library

--- cmake/Installing.cmake
+++ cmake/Installing.cmake
@@ -8,7 +8,7 @@ function(template_add_library)
     cmake_parse_arguments(TEMPLATE "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
     
     add_library(${TEMPLATE_LIB_NAME})
-    target_sources(${TEMPLATE_LIB_NAME} PRIVATE ${TEMPLATE_SRCS})
+    target_sources(${TEMPLATE_LIB_NAME} PRIVATE "${TEMPLATE_SRCS}")
     target_include_directories(${TEMPLATE_LIB_NAME}
         PRIVATE
             # where the library itself will look for its internal headers
@@ -24,29 +24,33 @@ function(template_add_library)
     set_target_properties(${TEMPLATE_LIB_NAME} PROPERTIES PUBLIC_HEADER "${TEMPLATE_PUBLIC_HDRS}")
 
     install(TARGETS ${TEMPLATE_LIB_NAME}
-        EXPORT "${TEMPLATE_LIB_NAME}Targets"
+        COMPONENT ${TEMPLATE_LIB_NAME}
+        EXPORT ${TEMPLATE_LIB_NAME}Targets
         PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${TEMPLATE_LIB_NAME}
         INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
     )
 
-    install(EXPORT "${TEMPLATE_LIB_NAME}Targets"
-        FILE "${TEMPLATE_LIB_NAME}Targets.cmake"
+    install(EXPORT ${TEMPLATE_LIB_NAME}Targets
+        COMPONENT ${TEMPLATE_LIB_NAME}
+        FILE ${TEMPLATE_LIB_NAME}Targets.cmake
         NAMESPACE ${TEMPLATE_LIB_NAMESPACE}::
         DESTINATION cmake
     )
 
     write_basic_package_version_file(
-        "${CMAKE_CURRENT_BINARY_DIR}/${TEMPLATE_LIB_NAME}ConfigVersion.cmake"
+        ${CMAKE_CURRENT_BINARY_DIR}/${TEMPLATE_LIB_NAME}ConfigVersion.cmake
         COMPATIBILITY AnyNewerVersion
     )
+
     configure_package_config_file(${PROJECT_SOURCE_DIR}/cmake/Config.cmake.in
-        "${CMAKE_CURRENT_BINARY_DIR}/${TEMPLATE_LIB_NAME}Config.cmake"
+        ${CMAKE_CURRENT_BINARY_DIR}/${TEMPLATE_LIB_NAME}Config.cmake
         INSTALL_DESTINATION cmake
     )
 
     install(FILES
-        "${CMAKE_CURRENT_BINARY_DIR}/${TEMPLATE_LIB_NAME}Config.cmake"
-        "${CMAKE_CURRENT_BINARY_DIR}/${TEMPLATE_LIB_NAME}ConfigVersion.cmake"
+        ${CMAKE_CURRENT_BINARY_DIR}/${TEMPLATE_LIB_NAME}Config.cmake
+        ${CMAKE_CURRENT_BINARY_DIR}/${TEMPLATE_LIB_NAME}ConfigVersion.cmake
         DESTINATION cmake
+        COMPONENT ${TEMPLATE_LIB_NAME}
     )
 endfunction()
