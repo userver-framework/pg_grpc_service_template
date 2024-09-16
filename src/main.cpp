00da4605922c90605427f2feeaddc755b80afeff
Anton Zhilin <antonyzhilin@userver.tech>
2024-09-16 19:11:27 +0300

    fix grpc: use grpc-client-common (#44)
    Co-authored-by: Antony Polukhin <antoshkka@userver.tech>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <userver/clients/http/component.hpp>
 #include <userver/components/minimal_server_component_list.hpp>
+#include <userver/congestion_control/component.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/ugrpc/client/client_factory_component.hpp>
+#include <userver/ugrpc/client/common_component.hpp>
+#include <userver/ugrpc/client/middlewares/deadline_propagation/component.hpp>
+#include <userver/ugrpc/client/middlewares/log/component.hpp>
+#include <userver/ugrpc/server/middlewares/congestion_control/component.hpp>
+#include <userver/ugrpc/server/middlewares/deadline_propagation/component.hpp>
+#include <userver/ugrpc/server/middlewares/log/component.hpp>
 #include <userver/ugrpc/server/server_component.hpp>
 #include <userver/utils/daemon_run.hpp>
 
@@ -13,8 +20,15 @@
 int main(int argc, char* argv[]) {
   auto component_list =
       userver::components::MinimalServerComponentList()
+          .Append<userver::congestion_control::Component>()
           .Append<userver::ugrpc::server::ServerComponent>()
+          .Append<userver::ugrpc::server::middlewares::congestion_control::Component>()
+          .Append<userver::ugrpc::server::middlewares::deadline_propagation::Component>()
+          .Append<userver::ugrpc::server::middlewares::log::Component>()
+          .Append<userver::ugrpc::client::CommonComponent>()
           .Append<userver::ugrpc::client::ClientFactoryComponent>()
+          .Append<userver::ugrpc::client::middlewares::deadline_propagation::Component>()
+          .Append<userver::ugrpc::client::middlewares::log::Component>()
           .Append<userver::server::handlers::Ping>()
           .Append<userver::components::TestsuiteSupport>()
           .Append<userver::components::HttpClient>()
